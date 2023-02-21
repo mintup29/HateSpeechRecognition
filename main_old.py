ff9126dc1dd2c2641ca06792ecf4636d1efc571a
mintup29 <vickodanendra@gmail.com>
2023-02-21 21:41:05 +0700

    first commit

---
+++ main_old.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import re
+import string
+import torch
+import torch.nn.functional as F
+from transformers import BertTokenizer
+
+
+punctuations = re.sub(r"[!<_>#:)\.]", "", string.punctuation)
+
+def punct2wspace(text):
+    return re.sub(r"[{}]+".format(punctuations), " ", text)
+
+def normalize_wspace(text):
+    return re.sub(r"\s+", " ", text)
+
+def casefolding(text):
+    return text.lower()
+
+# def classification(label):
+#     if label == 0:
+#         label = 'Negative'
+#     else:
+#         label = 'Positive'
+#     return label
+
+def predict(text):
+    text = punct2wspace(text)
+    text = normalize_wspace(text)
+    text = casefolding(text)
+
+    model = torch.load("model.pkl", map_location='cpu')
+
+    tokenizer = BertTokenizer.from_pretrained('indobenchmark/indobert-base-p1')
+    subwords = tokenizer.encode(text)
+    subwords = torch.LongTensor(subwords).view(1, -1).to(model.device)
+
+    logits = model(subwords)[0]
+    labels = [torch.topk(logit, k=1, dim=-1)[1].squeeze().item() for logit in logits]
+
+    # if labels == 0:
+    #     for i, label in enumerate(labels):
+    #         st.success(f'Label : NEGATIVE ({F.softmax(logits, dim=-1).squeeze()[label] * 100:.3f}%)')
+    # else:
+    #     for i, label in enumerate(labels):
+    #         st.warning(f'Label : POSITIVE ({F.softmax(logits, dim=-1).squeeze()[label] * 100:.3f}%)')
+
+    for i, label in enumerate(labels):
+        if label == 0:
+            st.success(f'Label : NEGATIVE ({F.softmax(logits, dim=-1).squeeze()[label] * 100:.3f}%)')
+            # st.write(f'Label : {classification(label)} ({F.softmax(logits, dim=-1).squeeze()[label] * 100:.3f}%)')
+        else:
+            st.warning(f'Label : POSITIVE ({F.softmax(logits, dim=-1).squeeze()[label] * 100:.3f}%)')
+
+
+st.title('Hate Speech Recognition')
+text = st.text_area('Tulis kalimat','..')
+if st.button('Analyze'):
+    if text is not None:
+      with st.spinner('Analyzing the text …'):
+        predict(text)
+else:
+     st.warning('Coba tulis Review')
+
+
